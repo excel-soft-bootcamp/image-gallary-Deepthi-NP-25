2c8dc8849531a9205482201991948d2277ef47e1
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-06-29 01:22:07 +0530

    Add files via upload

---
+++ ImageGalleryShare/Gmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGalleryShare
+{
+    public class Gmail : Iapplication
+    {
+        public void send()
+        {
+            Console.WriteLine("Image sent through Gmail");
+
+        }
+    }
+}
---
+++ ImageGalleryShare/ImageGallery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGalleryShare
+{
+    public class ImageGallery
+    {
+        Iapplication iapplication;
+
+        public ImageGallery(Iapplication gallery)
+        {
+            this.iapplication = gallery;
+        }
+
+        public void share()
+        {
+            iapplication.send();
+        }
+
+        
+    }
+}
